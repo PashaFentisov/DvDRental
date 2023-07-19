0007b9aa06e719b4d28015658e82f193e78d11b4
Pasha Fentisov <pasha16.ua@gmail.com>
2023-07-20 00:10:38 +0300

    #10[feature] DR-005: added Film entity, created two dtos - FilmDto and FilmSavingDto, created mappers for them and bound Film with Category as ManyToMany

--- src/main/java/com/pashonokk/dvdrental/entity/Category.java
+++ src/main/java/com/pashonokk/dvdrental/entity/Category.java
@@ -1,12 +1,12 @@
 package com.pashonokk.dvdrental.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -18,6 +18,22 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true, nullable = false, updatable = false)
     private String name;
     private LocalDate lastUpdate;
+    @ManyToMany(mappedBy = "categories", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Film> films = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return name.equals(category.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
